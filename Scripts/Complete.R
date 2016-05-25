0510c6f9dc7fe6925f5b31481070b2dddff9326d
Joseph L Crockett <jcrockett@ucmerced.edu>
2016-05-25 14:37:34 -0700

    Date with lubridate

--- Scripts/Complete.R
+++ Scripts/Complete.R
@@ -1,7 +1,8 @@
 #This script will serve to download and convert mtbs rasters into gridded products
 #Steps:
 #1) Download data from MTBS.gov
-#The file names are in a format "<Fire ID>_<prefire image date>_<postfire image date>_dnbr6.tif" or "<Fire ID>_<postfire image date>_nbr6.tif"
+#The file names are in a format "<Fire ID>_<prefire image date>_<postfire image date>_dnbr6.tif" or
+#"<Fire ID>_<postfire image date>_nbr6.tif"
 
 #1.5) strip projection data from the metadata.txt file
 
@@ -12,7 +13,10 @@
 #4) Identify pixels with the majority of each fire (note: most fires will not cover more than one pixel.  If they do, 
 
 #-----------load packages------------------------------------
-sapply(c("ggplot2", "raster", "sp","rgdal","reshape2"), ndpkg)
+sapply(
+  c("ggplot2","raster","sp","rgdal","reshape2","zoo","lubridate"),
+  ndpkg
+  )
 
 
 #-----------extract tifs from folders------------------------
@@ -29,8 +33,19 @@ for (j in rlist){
   r <- assign(unlist(strsplit(j, "[.]"))[1], raster(j))
   sr <- "+proj=longlat +ellps=WGS84 +no_defs"
   pr <-projectRaster(r, crs = sr, method = "ngb")
-  z <- projectRaster(from = pr, to = grdr, method = "ngb")
+  pr_dt <-as.Date(
+    ifelse(strsplit(names(projected_raster), "_")[[1]][3] == "nbr6",   #files are either dnbr6 or nbr6
+           strsplit(names(projected_raster), "_")[[1]][2],             #nbr6 files have one date, dnbr6 have two
+           strsplit(names(projected_raster), "_")[[1]][3]),            #getting the date from the file name. 
+     format = '%Y%m%d'
+  )
+  month(pr_dt)
+  year(pr_dt)
+  pr <- trim(pr)
+  pr[ pr == 6] <- NA                                                      #the value 6 is unprocessed data
     
+  z <- projectRaster(from = pr, to = grdr, method = "ngb")                #match resolution
+  
 }
 
 
@@ -55,20 +70,34 @@ sr2 <- "+proj=longlat +ellps=WGS84 +no_defs"
 # Project Raster, using nearest neighbor
 projected_raster <- projectRaster(r, crs = sr2, method = "ngb")
 plot(projected_raster)
-ras_pr <- rasterToPoints(projected_raster, Spatial = T)
 
-z <- overlay(projected_raster, grdr, fun=function(x,y){return(x+y)})
+
+projected_raster[projected_raster == 6] <- NA
+
+table(values(projected_raster))
+xmin(projected_raster)
 
 ras_pr <- data.frame(rasterToPoints(projected_raster))
 colnames(ras_pr) <- c("x", "y", "value")
+
 grdr_pr <- data.frame(rasterToPoints(grdr))
 
 head(grdr_pr)
 head(ras_pr)
 
-ggplot(ras_pr, aes(x = x, y = y, fill = value)) +geom_raster() 
+ras_pr$value <- ifelse(ras_pr$value == 0, NA, ras_pr$value)
+ras_pr$value <- ifelse(ras_pr$value == 6, NA, ras_pr$value)
+
+
+ggplot(ras_pr, aes(x = x, y = y, fill = value)) +geom_raster() + geom_hline(yintercept = grdr_pr[,2]) + geom_vline(xintercept = grdr_pr[,1]) 
 z <- projectRaster(from = projected_raster, to = grdr, method = "ngb")
 z_pr <- data.frame(rasterToPoints(z))
 colnames(z_pr) <- c("lon", "lat", "value")
 
-ggplot(z_pr, aes(x = lon, y = lat, fill = value)) +geom_raster() 
+ggplot(z_pr, aes(x = lon, y = lat, fill = value)) +geom_raster() +geom_hline(yintercept = z_pr[,2]) + geom_vline(xintercept = z_pr[,1])
+
+
+
+ifelse(strsplit(names(projected_raster), "_")[[1]][3] == "nbr6",
+       strsplit(names(projected_raster), "_")[[1]][2],
+       strsplit(names(projected_raster),"_")[[1]][3])
